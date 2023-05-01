450abc668488c45b83187a50b33eb752fc72dc7b
26-k <26nilk@gmail.com>
2023-05-01 22:22:46 +0900

    walljump作成中

---
+++ 2dAct/Assets/Character_Jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Jump : MonoBehaviour
+{
+    Player owner;
+    public void Init(Player owner)
+    {
+        this.owner = owner;
+    }
+    public void DoJump()
+    {
+        owner.moveData.isGravity = true;
+        owner.moveData.grav = owner.moveData.jumpPow;
+        owner.moveData.spd.y = owner.moveData.grav;
+        owner.moveData.isGround = false;
+        owner.moveData.jumpTime = 0.2f;
+    }
+
+    /// <summary>
+    /// 壁ジャン、通常ジャンプに加えて進行方向の反対向きに力が加わる
+    /// </summary>
+    public void DoWallJump()
+    {
+        DoJump();
+        owner.moveData.spd.x = 30; //てきとー
+
+    }
+}
--- 2dAct/Assets/Player.cs
+++ 2dAct/Assets/Player.cs
@@ -4,6 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+[SerializeField]
+public class MoveData
+{
+    [LabelText("移動速度")] public Vector2 spd; //別で持たせるべき
+    [LabelText("ジャンプ力")] public float jumpPow = 12.0f;
+    public bool isLeft = false;
+    public bool isGround = false;
+    public bool isGravity = false;
+    public float grav = 0.0f;
+    public int jumpCount = 0;
+    public float jumpTime = 0.0f;
+}
+
+
 public class Player : MonoBehaviour
 {
     const string LeftAnimName = "Left";
@@ -13,118 +27,165 @@ public class Player : MonoBehaviour
 
     [LabelText("アニメーション")] [SerializeField] Animator anim;
     [LabelText("拡大縮小アニメーション")] [SerializeField] Animator scaleAnim;
-    [LabelText("移動速度")] [SerializeField] Vector2 spd;
     [LabelText("移動速度")] [SerializeField] float moveSpd = 8.0f;
-    [LabelText("ジャンプ力")] [SerializeField] float jumpPow = 12.0f;
+    [LabelText("ジャンプ力(上書き)")] [SerializeField] float OverridejumpPow = 16.0f;
     [LabelText("砂煙")] [SerializeField] ParticleSystem dust;
     [LabelText("先行カメラ位置")] [SerializeField] GameObject leadingCameraObj;
     [LabelText("先行カメラ範囲")] [SerializeField] Vector2 leadingCameraMargin;
+    [LabelText("壁ジャン猶予時間")] [SerializeField] float wallJumpInterval = 0.2f;
     string nowAnimState = RightIdleAnimName;
+    bool isTouchWall = false;
+    float wallJumpTimer = 0.0f;
 
     Rigidbody2D rgd;
-    bool isLeft = false;
-    bool isGround = false;
-    bool isGravity = false;
-    float grav = 0.0f;
-    int jumpCount = 0;
-    float jumpTime = 0.0f;
+    public MoveData moveData = new MoveData();
+    [SerializeField] Character_Jump jump;
+
+    [SerializeField] GameObject testWallJumpObj;
 
     void Start()
     {
-        rgd = GetComponent<Rigidbody2D>();
+        if (rgd == null) //地形のアタッチ忘れ対策
+        {
+            rgd = GetComponent<Rigidbody2D>();
+        }
+        moveData.jumpPow = OverridejumpPow;
+        Init();
     }
 
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
     public void Init()
     {
-
+        jump.Init(this);
     }
 
     public void Update()
     {
+        //テスト用、壁ジャンが可能な時はオブジェクトを表示する
+
+        testWallJumpObj.SetActive(wallJumpTimer > 0.0f);
         var a = CheckGroundStatus();
-        jumpTime -= Time.deltaTime;
+        DecTimer();
         Debug.Log(a.collider);
         if (a.collider != null)
         {
-            if (a.collider.transform.tag == "Terrain" && jumpTime <= 0)
+            if (a.collider.transform.tag == "Terrain" && moveData.jumpTime <= 0) //床系統に触れている場合
             {
-                if (isGround == false)
+                if (moveData.isGround == false)
                 {
-                    Debug.Log("接地");
                     scaleAnim.Play("Ground");
                     dust.Play();
                 }
-                isGround = true;
-                spd.y = 0.0f;
-                jumpCount = 1;
-                grav = -3.0f;
+                moveData.isGround = true;
+                moveData.spd.y = 0.0f;
+                moveData.jumpCount = 1;
+                moveData.grav = -3.0f;
             }
             else
             {
-                isGround = false;
-                jumpCount = 0;
+                moveData.isGround = false;
+                moveData.jumpCount = 0;
             }
+        }
+
+
+        isTouchWall = false;
+        if (moveData.isGround == false)
+        {
+            var touchWall = CheckTouchWall();
+            if (touchWall.collider != null)
+            {
+                if (touchWall.collider.transform.tag == "Terrain" && moveData.jumpTime <= 0)//床系統に触れている場合
+                {
+                    wallJumpTimer = wallJumpInterval;
+                    isTouchWall = true;
+                }
+            }
+        }
+
+        if (wallJumpTimer > 0.0f && Input.GetKeyDown(KeyCode.Space)) //スペースが押された時壁ジャンチェック
+        {
+            jump.DoWallJump(); //test
         }
 
         if (Input.GetKey(KeyCode.Space)) //スペースキーが押されている時はジャンプ用の重力に
         {
-            if (isGround)
+            if (moveData.isGround)
             {
-                isGravity = true;
-                grav = jumpPow;
-                spd.y = grav;
-                isGround = false;
-                jumpTime = 0.2f;
-                scaleAnim.Play("JumpStart");
-                Debug.Log("ジャンプ");
-                dust.Play();
+                DoJump();
             }
         }
         else //押されていない時は降下用重力に
         {
-            if (isGravity == true)
+            if (moveData.isGravity == true)
             {
-                isGravity = false;
+                moveData.isGravity = false;
             }
         }
+        TryMove();
+        AnimeUpdate();
+    }
+
+    private void DecTimer()
+    {
+        moveData.jumpTime -= Time.deltaTime;
+        wallJumpTimer -= Time.deltaTime;
+    }
+
+    private void TryMove()
+    {
         if (Input.GetKey(KeyCode.A))
         {
-            spd.x = -moveSpd;
+            moveData.spd.x = -moveSpd;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            spd.x = moveSpd;
+            moveData.spd.x = moveSpd;
         }
-        else
+        else //キーを入力していない間の減速
         {
-            spd.x = spd.x * 0.5f;
+            float speedDecRatio = 0.95f;
+            moveData.spd.x = moveData.spd.x * speedDecRatio;
         }
-        AnimeUpdate();
+    }
+
+    private void DoJump()
+    {
+        jump.DoJump();
+        scaleAnim.Play("JumpStart");
+        dust.Play();
     }
 
     void FixedUpdate()
     {
-        spd.y = grav;
-        if (spd.y < -jumpPow)
+        moveData.spd.y = moveData.grav;
+        if (moveData.spd.y < -moveData.jumpPow)
         {
-            spd.y = -jumpPow;
+            moveData.spd.y = -moveData.jumpPow;
         }
-        if (isGravity)
+        if (moveData.isGravity)
         {
-            grav -= 0.75f;
+            moveData.grav -= 0.75f;
         }
         else
         {
-            grav -= 2.0f;
+            moveData.grav -= 2.0f;
         }
-        rgd.velocity = spd; //New
+        if (isTouchWall) //壁ずさり
+        {
+            moveData.grav = Mathf.Max(moveData.grav, -4.0f);
+        }
+        rgd.velocity = moveData.spd; //New
         if (rgd.velocity.x != 0)
         {
-            float rate = (spd.x / moveSpd);
+            float rate = (moveData.spd.x / moveSpd);
             Vector3 targetPos = Vector3.zero;
             targetPos.x = this.transform.position.x + rate * leadingCameraMargin.x;
             targetPos.y = this.transform.position.y;
-            leadingCameraObj.transform.position = Vector3.Lerp(leadingCameraObj.transform.position, targetPos,0.25f);
+            //先行カメラが気持ち悪いので一旦止める :Fix
+            //leadingCameraObj.transform.position = Vector3.Lerp(leadingCameraObj.transform.position, targetPos,0.25f);
         }
     }
 
@@ -133,17 +194,17 @@ public class Player : MonoBehaviour
         anim.SetFloat("MoveSpeed", Mathf.Abs(rgd.velocity.x * 0.5f));
         if (rgd.velocity.x < 0)
         {
-            isLeft = true;
+            moveData.isLeft = true;
             changeAnimeState(LeftAnimName);
         }
         else if (rgd.velocity.x > 0)
         {
-            isLeft = false;
+            moveData.isLeft = false;
             changeAnimeState(RightAnimName);
         }
         else
         {
-            if (isLeft)
+            if (moveData.isLeft)
             {
                 changeAnimeState(LeftIdleAnimName);
             }
@@ -163,6 +224,10 @@ public class Player : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 下方向に僅かなrayを飛ばして接地しているか判定する
+    /// </summary>
+    /// <returns></returns>
     RaycastHit2D CheckGroundStatus()
     {
         Vector2 startPos = (Vector2)transform.position;
@@ -170,7 +235,68 @@ public class Player : MonoBehaviour
         Vector2 pos = new Vector2(0, -0.55f);
         Vector2 size = new Vector2(0.3f, 0.05f);
         float distance = 0.1f;
-
         return Physics2D.BoxCast(startPos + pos, size, 0, Vector2.down, distance);
     }
+
+
+    /// <summary>
+    /// 向いている方向に僅かなrayを飛ばして壁にくっついてるか判定する。
+    /// </summary>
+    /// <returns></returns>
+    RaycastHit2D CheckTouchWall()
+    {
+        Vector2 startPos = (Vector2)transform.position;
+
+        Vector2 pos = new Vector2(0, -0.55f);
+        Vector2 size = new Vector2(0.3f, 0.05f);
+        float distance = 0.1f;
+        Vector2 targetDir = Vector2.right;
+        if (moveData.isLeft)
+        {
+            targetDir = Vector2.left;
+        }
+        return Physics2D.BoxCast(startPos + pos, size, 0, targetDir, distance);
+    }
+
+    #region 坂道チェックの名残だったもの
+    //void Test ()
+    //{
+    //    //rayの設定
+    //    Vector2 startPos = (Vector2)transform.position;
+    //    Vector2 pos = new Vector2(0, -0.55f);
+    //    Vector2 size = new Vector2(0.3f, 0.05f);
+    //    float distance = 0.1f;
+
+    //    //rayを飛ばして命中した左端と右端を取得する。
+    //    Vector2 leftVec = new Vector2(-0.3f, 0); //仮
+    //    var b = startPos + pos + leftVec;
+    //    var a = Physics2D.BoxCast(b, size, 0, Vector2.down, distance); //開始地点から少し左にずれた地形を取得する…?
+    //    Debug.Log($"LeftHit::{a.normal}");
+    //    var c = startPos + pos + (leftVec * -1.0f);
+    //    a = Physics2D.BoxCast(c, size, 0, Vector2.down, distance); //開始地点から少し→にずれた地形を取得する…?
+    //    Debug.Log($"RightHit::{a.normal}");
+
+    //}
+
+    //    /// <summary>
+    //    /// 坂道チェック用
+    //    /// 登るのが遅かったり勝手に下るのを修正
+    //    /// </summary>
+    //    /// <param name="startPos">対象位置</param>
+    //    /// <param name="dir">方向</param>
+    //    /// <returns></returns>
+    //    Vector2 SlopeCheck(Vector2 startPos, Vector2 dir)
+    //    {
+    //        Vector2 slopeVec = Vector2.right;
+    //        float Checkdistance = 1.0f;
+    //        RaycastHit2D rayHit2D = Physics2D.Raycast(startPos, Vector2.down, Checkdistance);
+
+    //        if (rayHit2D.collider != null)
+    //        {
+    //            //当たった地形の法線ベクトルを取る
+    //            Vector2 normal = rayHit2D.normal; //命中したrayの法線ベクトル
+
+    //        }
+    //    }
+    #endregion
 }
